c78919b47d2d5f4724d69c98acc9bd09cb4a108d
sebacaccaro <sebastianocaccaro@gmail.com>
2019-03-19 17:17:52 +0100

    Aggiornato MAGIC

--- Utility/MAGIC/MAGIC.sh
+++ Utility/MAGIC/MAGIC.sh
@@ -7,23 +7,23 @@ echo "Sto compilando i tex 3 volte (non si e' mai troppo sicuri)"
         do
             printf 'Complazione #'
             printf $i
-            cd ../../RP/Norme-di-Progetto/
+            cd ../../RQ/Norme-di-Progetto/
             pdflatex norme-di-progetto.tex --enable-installer > /dev/null
             printf "."
             cd ../../Utility/MAGIC/
-            cd ../../RP/Analisi_dei_requisiti/
+            cd ../../RQ/Analisi_dei_requisiti/
             pdflatex AnalisiDeiRequisiti.tex --enable-installer >/dev/null
             printf "."
             cd ../../Utility/MAGIC/
-            cd ../../RP/Glossario/
+            cd ../../RQ/Glossario/
             pdflatex Glossario.tex --enable-installer >/dev/null
             printf "."
             cd ../../Utility/MAGIC/
-            cd ../../RP/pdq/
+            cd ../../RQ/pdq/
             pdflatex pdq.tex --enable-installer >/dev/null
             printf "."
             cd ../../Utility/MAGIC/
-            cd ../../RP/Piano_di_Progetto/Documento/
+            cd ../../RQ/Piano_di_Progetto/Documento/
             pdflatex Piano_di_Progetto.tex --enable-installer >/dev/null
             printf "."
             cd ../../../Utility/MAGIC/
@@ -31,11 +31,11 @@ echo "Sto compilando i tex 3 volte (non si e' mai troppo sicuri)"
             echo
         done  
 
-perl gulpeasepdf.pl ../../RP/Norme-di-Progetto/norme-di-progetto.pdf "Scopo del documento" 1 Norme  >log.txt
-perl gulpeasepdf.pl ../../RP/Analisi_dei_requisiti/AnalisiDeiRequisiti.pdf "Scopo del documento" 2 Analisi >log.txt
-perl gulpeasepdf.pl ../../RP/Glossario/Glossario.pdf "Ambiente cloud" 1 Glossario >log.txt
-perl gulpeasepdf.pl ../../RP/pdq/pdq.pdf "Scopo del documento" 2 Pdq >log.txt
-perl gulpeasepdf.pl ../../RP/Piano_di_Progetto/Documento/Piano_di_Progetto.pdf "Scopo del documento" 2 Pdp >log.txt
+perl gulpeasepdf.pl ../../RQ/Norme-di-Progetto/norme-di-progetto.pdf "Scopo del documento" 1 Norme  >log.txt
+perl gulpeasepdf.pl ../../RQ/Analisi_dei_requisiti/AnalisiDeiRequisiti.pdf "Scopo del documento" 2 Analisi >log.txt
+perl gulpeasepdf.pl ../../RQ/Glossario/Glossario.pdf "Ambiente cloud" 1 Glossario >log.txt
+perl gulpeasepdf.pl ../../RQ/pdq/pdq.pdf "Scopo del documento" 2 Pdq >log.txt
+perl gulpeasepdf.pl ../../RQ/Piano_di_Progetto/Documento/Piano_di_Progetto.pdf "Scopo del documento" 2 Pdp >log.txt
 echo "Tranquillo, questi warning sono normali"
 
 echo "Sto mettendo i file nel foglio google"
